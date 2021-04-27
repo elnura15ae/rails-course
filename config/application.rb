f1fa9241bfd5b3786601033166cd735bf68c16a6
elnura15ae <elnura15ae@gmail.com>
2021-04-27 10:00:59 +0200

    update

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative "boot"
+
+require "rails/all"
+
+
+Bundler.require(*Rails.groups)
+
+module RailsCourse
+  class Application < Rails::Application
+    
+    config.load_defaults 6.1
+
+    
+  end
+end
